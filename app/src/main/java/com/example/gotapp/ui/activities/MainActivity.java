b253dbd7da7b5c51a58471cd2fbd748474bf64eb
Elena Space <elena.space@gmail.com>
2020-08-28 03:02:34 +0300

    fix

--- app/src/main/java/com/example/gotapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/gotapp/ui/activities/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.gotapp.ui.activities;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
@@ -14,11 +15,12 @@ import androidx.viewpager.widget.ViewPager;
 import com.example.gotapp.R;
 import com.example.gotapp.ui.fragments.PageFragment;
 import com.example.gotapp.viewmodels.CharacterViewModel;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private CharacterViewModel charViewModel;
 
@@ -28,19 +30,24 @@ public class MainActivity extends FragmentActivity {
     TabLayout tabLayout;
     ViewPager pager;
     PagerAdapter pagerAdapter;
+    DrawerLayout drawer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tabLayout = findViewById(R.id.sliding_tabs);
         pager = findViewById(R.id.pager);
+        drawer = findViewById(R.id.drawer);
         pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(pager);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
         pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
             @Override
